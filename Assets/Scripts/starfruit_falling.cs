6571593ce0ee332b64d695a1932a93a19491fd4c
SteamedHams3 <146746723+SteamedHams3@users.noreply.github.com>
2024-04-30 17:39:08 +0100

    starfruit falling interaction

---
+++ Assets/Scripts/starfruit_falling.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class starfruit_falling : MonoBehaviour
+{
+   
+  public Animator animator;
+  public AudioClip starfruitFall;
+  public AudioClip pirateReaction;
+   
+
+   bool will_play_fruit_sound = false;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player" && !will_play_fruit_sound)
+        {
+           StartCoroutine(starfruitDrop());
+        }
+    }
+
+    IEnumerator starfruitDrop()
+    {
+         animator.SetTrigger("FruitFalling");
+         yield return new WaitForSeconds(0.5f);
+         gameObject.GetComponent<AudioSource>().PlayOneShot(starfruitFall);
+         yield return new WaitForSeconds(0.5f);
+          gameObject.GetComponent<AudioSource>().PlayOneShot(pirateReaction);
+         will_play_fruit_sound = true;
+    }
+}
